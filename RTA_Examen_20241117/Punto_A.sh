f1dae490ee46e943d23ec404ed126d5ef85d74d5
luca <luca@Luca>
2024-11-17 13:07:55 +0000

    scripts

---
+++ RTA_Examen_20241117/Punto_A.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+DISCO_1=$(lsblk | grep 1G | awk '{print $1}')
+DISCO_2=$(lsblk | grep 2G | awk '{print $1}' | head -n1)
+DISCO_1G=$DISCO_1
+DISCO_2G=$DISCO_2
+
+echo "particionamos los discos ${DISCO_1G}/${DISCO_2G} y le damos formato LVM..."
+
+sudo fdisk /dev/$DISCO_1G << EOF
+n
+p
+1
+
+
+t
+8E
+w
+EOF
+
+sudo fdisk /dev/$DISCO_2G << EOF
+n
+p
+1
+
+
+t
+8E
+w
+EOF
+
+echo "limpiamos las particiones de basura..."
+
+sudo wipefs -a /dev/${DISCO_1G}1
+sudo wipefs -a /dev/${DISCO_2G}1
+
+echo "creacion de pvs"
+
+sudo pvcreate /dev/${DISCO_1G}1
+sudo pvcreate /dev/${DISCO_2G}1
+
+echo "creacion de vgs"
+
+sudo vgcreate vg_datos /dev/${DISCO_2G}1
+sudo vgcreate vg_temp /dev/${DISCO_1G}1
+
+echo "creacion de lvs"
+
+sudo lvcreate -L 5M vg_datos -n lv_docker
+sudo lvcreate -L 1.5G vg_datos -n lv_workareas
+sudo lvcreate -L 512M vg_temp -n lv_swap
+
+echo "le damos formato a la memoria swap..."
+
+sudo mkswap /dev/mapper/vg_temp-lv_swap
+sudo swapon /dev/mapper/vg_temp-lv_swap
+
+echo "formateamos..."
+
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+
+echo "montamos las particiones de lvm..."
+
+sudo mkdir /work/
+sudo mount /dev/mapper/vg_datos-lv_workareas /work/
+sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
+
+echo "restarteamos docker..."
+
+sudo systemctl restart docker
